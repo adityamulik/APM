f2550f55ee7388f33c91761ab0a55cf6f801a336
Aditya Mulik <adityamulik@Adityas-MacBook-Pro.local>
2019-04-23 05:45:54 +0530

    Data Binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
 })
 
 export class ProductListComponent {
-
+  pageTitle: string = 'Product List!';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  products: any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "https://images.homedepot-static.com/productImages/420a9288-cca7-4248-8f22-bd8664220d34/svn/pure-garden-wheelbarrows-hw1500113-64_1000.jpg"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "https://images.homedepot-static.com/productImages/c275d0bb-5e98-412c-b1b1-8726fd1f1477/svn/dewalt-claw-hammers-dwht51054-64_1000.jpg"
+    }
+  ];
+  toggleImage(): void{
+    this.showImage = !this.showImage;
+  }
 }
