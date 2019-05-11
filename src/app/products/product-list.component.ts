1d840e20e56f1902f868f93a88f79208caf8fb73
Aditya Mulik <adityamulik@Adityas-MacBook-Pro.local>
2019-05-11 20:02:48 +0530

    Nested components

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -25,6 +25,16 @@ export class ProductListComponent implements OnInit {
   filteredProducts: IProduct[];
   products: IProduct[] = [
     {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
       "productId": 2,
       "productName": "Garden Cart",
       "productCode": "GDN-0023",
@@ -43,12 +53,36 @@ export class ProductListComponent implements OnInit {
       "price": 8.9,
       "starRating": 4.8,
       "imageUrl": "https://images.homedepot-static.com/productImages/c275d0bb-5e98-412c-b1b1-8726fd1f1477/svn/dewalt-claw-hammers-dwht51054-64_1000.jpg"
+    },
+    {
+      "productId": 8,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2016",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+    },
+    {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2015",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
     }
   ];
 
   constructor(){
     this.filteredProducts = this.products;
     this.listFilter = 'cart';
+  }
+
+  onRatingClicked(message: string): void{
+    this.pageTitle = 'Product List: ' +message;
   }
 
   performFilter(filterBy: string): IProduct[] {
